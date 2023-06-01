1db96f13106129e1095f40180896f08548fd512e
Francis Ssemwanga <122269670+f-ssemwanga@users.noreply.github.com>
2023-06-01 12:23:54 +0100

    completed styling banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,9 @@
 export const Banner = () => {
     return (
-      <div>Banner</div>);
+      <div className="border border-primary-subtle m-3 p-3 text-center">
+        <h1>My Movies</h1>
+        <h2 className="fs-5">List of my fav movies</h2>
+
+      </div>);
   }
   
